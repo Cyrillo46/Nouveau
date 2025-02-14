8c6d2e671698d05086e9340087a2ffdd7fa38f9b
Cyrillo46 <yahooisbetter46@gmail.com>
2025-02-13 22:45:20 -0500

    added useNavigate, form improvements, and notes

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,18 @@
+import { useState } from "react";
 import { GiGriffinSymbol } from "react-icons/gi";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 export const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("email submitted");
+    console.log("password submitted");
+
+    navigate("/dashboard");
+  };
   return (
     <div className=" bg-blue-100 h-screen flex flex-col items-center">
       <h1 className="text-8xl mb-6 mt-2 text-blue-600">Welcome!</h1>
@@ -9,16 +21,31 @@ export const Login = () => {
           <GiGriffinSymbol size={450} color="#7F1D1D" />
         </div>
       </div>
-      <form className="flex flex-col">
-        <input type="text" />
-        <label className="bg-blue-500 text-blue-50 p-1 text-center rounded-md">
+      {/* connect to  backend api */}
+      <form handleSubmit={handleSubmit} className="flex flex-col">
+        <label className="bg-blue-500 text-blue-50 p-1 text-center mt-2.5 rounded-md">
           Email
         </label>
-        <input type="text" className="" />
-        <label className="bg-blue-500 text-blue-50 p-1 text-center rounded-md">
+        <input
+          onChange={(e) => setEmail(e.target.value)}
+          type="text"
+          className="mb-4 border rounded-md"
+        />
+
+        <label className="bg-blue-500 text-blue-50 p-1 text-center mt-2.5 mb-4 rounded-md">
           Password
         </label>
+        <input
+          onChange={(e) => setPassword(e.target.value)}
+          type="text"
+          className="border"
+        />
       </form>
+      <button
+        type="submit"
+        className="bg-blue-500 text-blue-50 p-1 text-center mt-2.5 mb-4 rounded-md cursor-pointer">
+        Login
+      </button>
       <Link to="/signup" className="">
         Need an account? Click here!
       </Link>
